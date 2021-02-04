a4ed648cd35ada3df3c40f6c4c5df6a2b03c131a
273494627 <273494627@qq.com>
2021-02-04 17:25:10 +0800

    删除不要项目

--- VBNet.DSGComm/FrmShowErr.vb
+++
@@ -1,5 +0,0 @@
-Option Strict Off
-Option Explicit On
-Friend Class FrmShowErr
-	Inherits System.Windows.Forms.Form
-End Class
--- VBNet.DSGComm/frmInfo.vb
+++
@@ -1,6 +0,0 @@
-Option Strict Off
-Option Explicit On
-Friend Class frmInfo
-	Inherits System.Windows.Forms.Form
-
-End Class
